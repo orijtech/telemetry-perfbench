dbd63f652e12958e56e673ff586463d6a7d6ce42
Nathan Dias <nathan.dias@orijtech.com>
2020-01-31 23:53:20 -0600

    use a pointer to logger in zerolog

--- zerolog_test.go
+++ zerolog_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rs/zerolog"
 )
 
-func A_log_zerolog(logger zerolog.Logger, a int) int {
+func A_log_zerolog(logger *zerolog.Logger, a int) int {
 	if a > 0 {
 		logger.Info().Msgf("a > 0 where a=%d", a)
 		_ = 10 * 12
@@ -17,7 +17,7 @@ func A_log_zerolog(logger zerolog.Logger, a int) int {
 	return B_log_zerolog(logger, "Called from A")
 }
 
-func B_log_zerolog(logger zerolog.Logger, b string) int {
+func B_log_zerolog(logger *zerolog.Logger, b string) int {
 	b = strings.ToUpper(b)
 	logger.Info().Msgf("b uppercased, so lowercased where len_b=%d", len(b))
 	if len(b) > 1024 {
@@ -31,6 +31,6 @@ func BenchmarkLoggingZerolog(b *testing.B) {
 	logger := zerolog.New(ioutil.Discard)
 	b.ResetTimer()
 	RunBenchmark(b, func(a int) int {
-		return A_log_zerolog(logger, a)
+		return A_log_zerolog(&logger, a)
 	})
 }
